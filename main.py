4697b6ca93ca7a3118c1f4a1f02e2c824b5c1393
Justin Lee <justinlsoftware@gmail.com>
2026-04-29 01:23:26 -0400

    Add samples for wip testing.

--- main.py
+++ main.py
@@ -4,7 +4,6 @@ import processor
 def main():
 	fpath = "/Users/jihoonlee/Downloads/wargreymon.mp4"
 	processor.video_ascii_to_json(fpath, "output.json")
-	processor.render_json_video("output.json")
 
 
 if __name__ == "__main__":
