adaaab8c7d4d93b237c2f956bfd116a87c1751d0
Justin Lee <justinlsoftware@gmail.com>
2026-04-28 22:50:03 -0400

    Add a function to calculate ascii per frame.

--- main.py
+++ main.py
@@ -1,3 +1,4 @@
+import json
 import sys
 import time
 
@@ -5,27 +6,36 @@ import cv2
 import numpy
 
 
-ASCII_RAMP = " .:-=+*#%@"
 ASCII_RAMP = " ░▒▓█"
 ASCII_RAMP_LEN = len(ASCII_RAMP) - 1
 LOOKUP = numpy.array(list(ASCII_RAMP))
-CLAHE = cv2.createCLAHE(clipLimit=6.0, tileGridSize=(8, 8))
 
 
-def frame_to_ascii(frame, width: int = 50, height: int = 25) -> str:
+def _frame_to_ascii(
+	frame: numpy.ndarray,
+	width: int,
+	height: int
+):
 	frame = cv2.resize(frame, (width, height))
 	frame = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
-	frame = CLAHE.apply(frame)
 	indices = (frame / 255 * ASCII_RAMP_LEN).astype(int)
-	return "\n".join("".join(row) for row in LOOKUP[indices])
+	return "\n".join(("".join(row) for row in LOOKUP[indices]))
 
 
-def img_to_ascii(fpath: str, width: int = 50, height: int = 25) -> str:
-	img = cv2.imread(fpath)
-	return frame_to_ascii(img, width, height)
+def image_to_ascii(
+	fpath: str,
+	width: int = 60,
+	height: int = 30
+):
+	frame = cv2.imread(fpath)
+	return _frame_to_ascii(frame, width, height)
 
 
-def video_to_ascii(fpath: str, width: int = 50, height: int = 25) -> None:
+def video_to_ascii(
+	fpath: str,
+	width: int = 60,
+	height: int = 30
+):
 	cap = cv2.VideoCapture(fpath)
 	fps = cap.get(cv2.CAP_PROP_FPS)
 	delay = 1 / fps
@@ -36,18 +46,38 @@ def video_to_ascii(fpath: str, width: int = 50, height: int = 25) -> None:
 			break
 		start = time.time()
 		sys.stdout.write("\033[H")
-		sys.stdout.write(frame_to_ascii(frame, width, height))
+		sys.stdout.write(_frame_to_ascii(frame, width, height))
 		sys.stdout.flush()
 		elapsed = time.time() - start
 		time.sleep(max(0, delay - elapsed))
 	cap.release()
 
 
-def main():
-	# video_to_ascii("wargraymon.mov", 120, 30)
-	test = img_to_ascii("agumon.webp", 120, 30)
-	print(test)
+def video_to_json(
+	fpath: str,
+	output: str,
+	width: int = 60,
+	height: int = 30
+):
+	import json
+	cap = cv2.VideoCapture(fpath)
+	with open(output, "w") as f:
+		f.write("[")
+		first = True
+		while True:
+			ret, frame = cap.read()
+			if not ret:
+				break
+			if not first:
+				f.write(",")
+			json.dump(_frame_to_ascii(frame, width, height), f)
+			first = False
+		f.write("]")
+	cap.release()
 
+
+def main():
+	video_to_json("video.mp4", "test.json")
 
 
 if __name__ == "__main__":
