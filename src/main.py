80e2926cede80188d3dc88f6667ae4cb1dea922c
Justin Lee <justinlsoftware@gmail.com>
2026-04-30 13:27:52 -0400

    Update frame height and width.

--- src/main.py
+++ src/main.py
@@ -2,6 +2,10 @@ import processor
 
 
 def main():
+	fpath = "/Users/jihoonlee/Movies/DaVinci Resolve/warp digivolve wargreymon.mov"
+	processor.video_ascii_to_json(fpath, "../assets/digimon/output.json")
+	# processor.render_json_video("../assets/digimon/output.json")
+
 	# fpath = "/Users/jihoonlee/Downloads/test.png"
 	# processor.image_ascii_to_json(fpath, "test.json")
 	# processor.video_ascii_to_json(fpath, "output.json")
