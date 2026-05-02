e7912609ef50f96af073f9fe05059f9dc9b40a1c
Justin Lee <justinlsoftware@gmail.com>
2026-05-01 22:37:32 -0400

    Add samples.

--- src/main.py
+++ src/main.py
@@ -2,7 +2,11 @@ import processor
 
 
 def main():
-	fpath = "/Users/jihoonlee/Movies/DaVinci Resolve/warp digivolve wargreymon.mov"
+	fpath = "/Users/jihoonlee/Pictures/vlcsnap-2026-05-01-00h15m47s927.png"
+	# fpath = "/Users/jihoonlee/Downloads/test.jpg"
+	ascii = processor.image_to_ascii(fpath)
+	print(ascii)
+	# fpath = "/Users/jihoonlee/Movies/DaVinci Resolve/warp digivolve wargreymon.mov"
 	processor.video_ascii_to_json(fpath, "../assets/digimon/output.json")
 	# processor.render_json_video("../assets/digimon/output.json")
 
