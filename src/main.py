5e7f3897b8b7fa2ddf990b2ae79c4b223c9ff358
Justin Lee <justinlsoftware@gmail.com>
2026-05-01 22:48:14 -0400

    Update animation.

--- src/main.py
+++ src/main.py
@@ -2,17 +2,8 @@ import processor
 
 
 def main():
-	fpath = "/Users/jihoonlee/Pictures/vlcsnap-2026-05-01-00h15m47s927.png"
-	# fpath = "/Users/jihoonlee/Downloads/test.jpg"
-	ascii = processor.image_to_ascii(fpath)
-	print(ascii)
-	# fpath = "/Users/jihoonlee/Movies/DaVinci Resolve/warp digivolve wargreymon.mov"
-	processor.video_ascii_to_json(fpath, "../assets/digimon/output.json")
-	# processor.render_json_video("../assets/digimon/output.json")
-
-	# fpath = "/Users/jihoonlee/Downloads/test.png"
-	# processor.image_ascii_to_json(fpath, "test.json")
-	# processor.video_ascii_to_json(fpath, "output.json")
+	fpath = "/Users/jihoonlee/Movies/wargreymon.mov"
+	processor.video_ascii_to_json(fpath, "../assets/wargreymon.json")
 
 
 if __name__ == "__main__":
