bee3998ead521010f6e8e182429e90f9db2bb670
Justin Lee <justinlsoftware@gmail.com>
2026-05-05 00:50:38 -0400

    Update colors.

--- src/main.py
+++ src/main.py
@@ -2,9 +2,9 @@ import processor
 
 
 def main():
-	fpath = "/Users/jihoonlee/Movies/wargreymon.mov"
-	processor.video_ascii_to_json(fpath, "../assets/digimon/output.json")
+    fpath = "/Users/jihoonlee/Movies/wargreymon.mov"
+    processor.video_ascii_to_json(fpath, "../assets/digimon/output.json")
 
 
 if __name__ == "__main__":
-	main()
+    main()
